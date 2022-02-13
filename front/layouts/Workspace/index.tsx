c5951a4d9b6e515e6044730234776e6d4807115c
developer-study <developer.jws.study@gmail.com>
2022-02-13 23:54:17 +0900

    workspace

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -3,6 +3,18 @@ import axios from 'axios';
 import React, { FC, useCallback } from 'react';
 import { Redirect } from 'react-router';
 import useSWR from 'swr';
+import gravatar from 'gravatar';
+import {
+  Header,
+  RightMenu,
+  ProfileImg,
+  WorkspaceWrapper,
+  Workspaces,
+  Channels,
+  Chats,
+  WorkspaceName,
+  MenuScroll,
+} from '@layouts/Workspace/styles';
 
 const Workspace: FC = ({ children }) => {
   const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
@@ -22,8 +34,22 @@ const Workspace: FC = ({ children }) => {
 
   return (
     <div>
-      <button onClick={onLogout}>로그아웃</button>
-      {children}
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
+      {/* <button onClick={onLogout}>로그아웃</button> */}
+      <WorkspaceWrapper>
+        <Workspaces>Workspaces</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>menuscroll</MenuScroll>
+        </Channels>
+        <Chats>chats</Chats>
+      </WorkspaceWrapper>
     </div>
   );
 };
