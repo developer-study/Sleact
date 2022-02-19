64506d3ab018161165dd7e0ee7610df44fe3f62e
developer-study <developer.jws.study@gmail.com>
2022-02-20 00:17:49 +0900

    direct message

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,11 +1,54 @@
-import Workspace from '@layouts/Workspace';
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Container, Header } from '@pages/DirectMessage/styles';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import { useParams } from 'react-router';
+import ChatList from '@components/ChatList';
+import ChatBox from '@components/ChatBox';
+import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { IDM } from '@typings/db';
 
 const DirectMessage = () => {
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+  const [chat, onChangeChat, setChat] = useInput('');
+  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
+    `/api/workspaces/${workspace}/dms/${id}/chat?perPage=20&page=1`,
+    fetcher,
+  );
+
+  const onSubmitForm = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      console.log(chat);
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            mutateChat();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat, workspace, id],
+  );
+
+  if (!userData || !myData) return null;
   return (
-    <Workspace>
-      <div>DirectMessage</div>
-    </Workspace>
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
+    </Container>
   );
 };
 
