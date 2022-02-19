db02a100352e5744a589e47a0730f0c803af6ad5
developer-study <developer.jws.study@gmail.com>
2022-02-20 02:46:33 +0900

    chat list

---
+++ front/components/ChatList/index.tsx
@@ -0,0 +1,19 @@
+import React, { VFC } from 'react';
+import { ChatZone, Section } from '@components/ChatList/styles';
+import { IDM } from '@typings/db';
+import Chat from '@components/Chat';
+
+interface Props {
+  chatData?: IDM[];
+}
+const ChatList: VFC<Props> = ({ chatData }) => {
+  return (
+    <ChatZone>
+      {chatData?.map((chat) => (
+        <Chat key={chat.id} data={chat} />
+      ))}
+    </ChatZone>
+  );
+};
+
+export default ChatList;
