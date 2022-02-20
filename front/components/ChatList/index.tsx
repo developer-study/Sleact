74f1ba03aeec18cab6ca2dbec067f8afc16ad4c5
developer-study <developer.jws.study@gmail.com>
2022-02-21 01:21:32 +0900

    chat list

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,24 +1,44 @@
-import React, { useCallback, useRef, VFC } from 'react';
+import React, { useCallback, useRef, VFC, forwardRef, MutableRefObject } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
-import { ChatZone, Section } from '@components/ChatList/styles';
+import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
 
 interface Props {
-  chatData?: IDM[];
+  chatSections: { [key: string]: IDM[] };
+  setSize: (f: (size: number) => number) => Promise<IDM[][] | undefined>;
+  isEmpty: boolean;
+  isReachingEnd: boolean;
 }
-const ChatList: VFC<Props> = ({ chatData }) => {
-  const scrollbarRef = useRef(null);
-  const onScroll = useCallback(() => {}, []);
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isEmpty, isReachingEnd }, scrollRef) => {
+  const onScroll = useCallback((values) => {
+    if (values.scrollTop === 0 && !isReachingEnd) {
+      setSize((prevSize) => prevSize + 1).then(() => {
+        const current = (scrollRef as MutableRefObject<Scrollbars>)?.current;
+        if (current) {
+          current.scrollTop(current.getScrollHeight() - values.scrollHeight);
+        }
+      });
+    }
+  }, []);
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
-        {chatData?.map((chat) => (
-          <Chat key={chat.id} data={chat} />
-        ))}
+      <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll}>
+        {Object.entries(chatSections).map(([date, chats]) => {
+          return (
+            <Section className={`section-${date}`} key={date}>
+              <StickyHeader>
+                <button>{date}</button>
+              </StickyHeader>
+              {chats.map((chat) => (
+                <Chat key={chat.id} data={chat} />
+              ))}
+            </Section>
+          );
+        })}
       </Scrollbars>
     </ChatZone>
   );
-};
+});
 
 export default ChatList;
