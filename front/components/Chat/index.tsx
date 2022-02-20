db2e8701cc0cbae259f1d6679676a283fea0319f
developer-study <developer.jws.study@gmail.com>
2022-02-20 17:25:31 +0900

    scrollbar, dayjs

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,4 +1,5 @@
 import React, { VFC } from 'react';
+import dayjs from 'dayjs';
 import gravatar from 'gravatar';
 import { ChatWrapper } from './styles';
 import { IDM } from '@typings/db';
@@ -17,7 +18,7 @@ const Chat: VFC<Props> = ({ data }) => {
       <div className="chat-text">
         <div className="chat-user">
           <b>{user.nickname}</b>
-          <span>{data.createdAt}</span>
+          <span>&nbsp;{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
         <p>{data.content}</p>
       </div>
